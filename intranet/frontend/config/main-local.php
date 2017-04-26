7d1c59f3d579baf444e6f1a07c244c77b05b1241
Julio Murillo <Julio Murillo@localhost>
2017-04-26 16:26:10 -0400

    Agregando archivos

---
+++ intranet/frontend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'z8_3wI4hyJTTScnJPbogYM_y-7Dd5HxC',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ src/web/intranet/common/components/GDhelper.php
@@ -0,0 +1,25 @@
+<?php
+namespace common\components;
+
+use Yii;
+
+class GDhelper{
+    
+    /**
+     * Return Username
+     * @return string
+     */
+    public static function getUsername() {
+        $identity = Yii::$app->user->getIdentity();
+        return $identity['username'];
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public static function getEmail() {
+        $identity = Yii::$app->user->getIdentity();
+        return $identity['email'];
+    }
+}
