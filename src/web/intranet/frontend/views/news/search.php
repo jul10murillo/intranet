36bf944780f35e1a499dc1d811eb9500d2be940f
ylopez <ylopez@localhost>
2017-08-10 08:33:19 -0400

    Ajustes responsive

--- src/web/intranet/frontend/views/news/search.php
+++ src/web/intranet/frontend/views/news/search.php
@@ -6,14 +6,20 @@ use yii\widgets\LinkPager;
 
 ?>
 <div class="search">
-    <?php $form = ActiveForm::begin(['action' => ['news/search']]); ?>
+    <?php $form = ActiveForm::begin(['action' => ['news/search'],'options' => [
+                'class' => 'form-inline'
+             ]]); ?>
+    <div class="col-sm-10 col-xs-10">   
         <?= Html::input('text','search',null,['placeholder'=>'  Buscar...'])?>
+    </div>
+    <div class="col-sm-2 col-xs-2">
         <?= Html::submitButton('<span class="glyphicon glyphicon-search"></span>') ?>
+    </div>    
     <?php ActiveForm::end(); ?>
 </div>
 <hgroup>
-    <h2 class="searchhead">Resultado de la busqueda: "<?= $search ?>"</h2>
-    <h2 class="lead"><strong class="text-danger"><?= $count ?></strong> <?= ($count > 1) ? "resultados encontrados" : "resultado encontrado" ?> de la busqueda de <strong class="text-danger"><?= $search ?></strong></h2>
+    <h2 class="searchhead">Resultado de la búsqueda: "<?= $search ?>"</h2>
+    <h2 class="lead"><strong class="text-danger"><?= $count ?></strong> <?= ($count > 1) ? "resultados encontrados" : "resultado encontrado" ?> de la búsqueda de <strong class="text-danger"><?= $search ?></strong></h2>
 </hgroup>
 <br>
 <section class="col-xs-12 col-sm-6 col-md-12">
